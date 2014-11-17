bd3659368746b89c490a43e4854e9224abb0f6fd
MaximHordijchuk <hordijchuk.m.i@gmail.com>
2014-11-17 22:31:33 +0200

    Changed string pattern

--- src/LexemeAnalyzer/LexemeType.java
+++ src/LexemeAnalyzer/LexemeType.java
@@ -9,17 +9,17 @@ import java.util.regex.Pattern;
 public enum LexemeType {
     COMMENT("comment", "(//.*)|(/\\*(.|\\s)*?((\\*/)|($)))", "#828282"),
     NUMBER("number", "(?<![a-zA-Z0-9_$])" +
-            "((0|([1-9][0-9]*)|(0(x|X)[a-fA-F0-9]*)|(0[1-7][0-7]*))((?i)(ul|lu|u|l)(?-i))?" +
-            "|([0-9]*\\.[0-9]+)([eE][-+]?[0-9]+)?)" +
+            "(([0-9]+([eE][-+]?[0-9]+))|([0-9]*\\.[0-9]*)([eE][-+]?[0-9]+)?" +
+            "|(0|([1-9][0-9]*)|(0(x|X)[a-fA-F0-9]*)|(0[1-7][0-7]*))((?i)(ul|lu|u|l)(?-i))?)" +
             "(?![a-zA-Z0-9_$])", "#1C86EE"),
     STRING_OR_CHARACTER("string or character",
-            "((\"(\\\\['\"\\\\0abfnrtuUxv]|[^\\\\\"\n]*)\")" +
-                    "|(\'((([^'\\\\\n]|\\\\['\"\\\\0abfnrtuUxv])|(\\\\x[0-9a-fA-F]{1,4})))?\'))(?![a-zA-Z0-9_$])",
+            "((\"((\\\\['\"\\\\0abfnrtuUxv\\?]|[^\\\\\"\n]|(\\\\x[0-9a-fA-F]{1,4})|(\\\\[0-7]{1,3}))*)\")" +
+                    "|(\'((([^'\\\\\n]|\\\\['\"\\\\0abfnrtuUxv\\?])|(\\\\x[0-9a-fA-F]{1,4}|(\\\\[0-7]{1,3}))))?\'))(?![a-zA-Z0-9_$])",
             "#6A8751"),
     RESERVED_WORD("reserved word", "(?<![a-zA-Z0-9_$])(int|double|void|char|long|unsigned|short|break|continue" +
             "|return|switch|and|or|xor|class|struct|enum|private|public|protected|static|template|for|if" +
             "|friend|const|new|delete)(?![a-zA-Z0-9_$])", "#FF8C00"),
-    SEPARATOR("separator", "[\\Q{}[]().,:;\\E]", "#DBCA57"),//#EE6363
+    SEPARATOR("separator", "[\\Q{}[]().,:;\\E]", "#DBCA57"),
     OPERATOR("operator",
             "<?<=?|>?>=?|->|\\*=?|/=?|%=?|&=?|\\|=?|\\^=?|\\+\\+|--|&&?|\\|(\\|)?|<<|>>|==?|!=?|\\+=?|-=?", "#008066"),
     PREPROCESSOR_DIRECTIVES("preprocessor directives", "#.*", "#66B008"),
